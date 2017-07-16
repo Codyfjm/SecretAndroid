b451b3b0f5ae99be198bfd6d929f3c00bc7405c9
cody <629523179@qq.com>
2017-07-16 17:29:23 +0800

    1.更改服务器地址，昨晚两部电脑无法ping通，今天可以，初步鉴定是网络本身的问题 2. 登陆验证，增加服务器端token验证加上账号缓存

--- Secret/src/com/codyfjm/secret/Config.java
+++ Secret/src/com/codyfjm/secret/Config.java
@@ -11,6 +11,7 @@ public class Config {
 	public static final String KEY_TOKEN = "token";
 	public static final String KEY_ACTION = "action";
 	public static final String KEY_PHONE_NUM = "phone";
+	public static final String KEY_PHONE_MD5 = "phone_md5";
 	public static final String KEY_STATUS = "status";
 	
 	public static final int RESULT_STATUS_SUCCESS = 1;
@@ -22,6 +23,10 @@ public class Config {
 	public static final String CHARSET = "utf-8";
 	
 	public static final String ACTION_GET_CODE = "send_pass";
+
+	public static final String ACTION_LOGIN = "login";
+
+	public static final String KEY_CODE = "code";
 	
 	public static String getCachedToken(Context context) {
 		return context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).getString(KEY_TOKEN, null);
@@ -30,6 +35,16 @@ public class Config {
 	public static void cacheToken(Context context,String token) {
 		Editor editor = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit();
 		editor.putString(KEY_TOKEN, token);
+		editor.commit();
+	}
+	
+	public static String getCachedPhoneNum(Context context) {
+		return context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).getString(KEY_PHONE_NUM, null);
+	}
+	
+	public static void cachePhoneNum(Context context,String phoneNum) {
+		Editor editor = context.getSharedPreferences(APP_ID, Context.MODE_PRIVATE).edit();
+		editor.putString(KEY_PHONE_NUM, phoneNum);
 		editor.commit();
 	}
 	
--- Secret/src/com/codyfjm/secret/atys/AtyLogin.java
+++ Secret/src/com/codyfjm/secret/atys/AtyLogin.java
@@ -2,6 +2,7 @@ package com.codyfjm.secret.atys;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -9,12 +10,15 @@ import android.view.View.OnClickListener;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.codyfjm.secret.Config;
 import com.codyfjm.secret.R;
 import com.codyfjm.secret.net.GetCode;
+import com.codyfjm.secret.net.Login;
+import com.codyfjm.secret.tools.MD5Tool;
 
 public class AtyLogin extends Activity {
 	
-	private EditText etPhone;
+	private EditText etPhone,etCode;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,9 @@ public class AtyLogin extends Activity {
 		setContentView(R.layout.aty_login);
 		
 		etPhone = (EditText) findViewById(R.id.etPhoneNum);
+		etCode = (EditText) findViewById(R.id.etCode);
 		
+		//获取验证码  
 		findViewById(R.id.btnGetCode).setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -46,10 +52,53 @@ public class AtyLogin extends Activity {
 					@Override
 					public void onFail() {
 						pdDialog.dismiss();
-						Toast.makeText(AtyLogin.this, "验证码不能为空", Toast.LENGTH_LONG).show();
+						Toast.makeText(AtyLogin.this, "验证码获取失败", Toast.LENGTH_LONG).show();
 					}
 				});
 			}
 		});
+		
+		//登陆验证
+		findViewById(R.id.btnLogin).setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				//手机号判空处理       
+				if (TextUtils.isEmpty(etPhone.getText().toString())) {
+					Toast.makeText(AtyLogin.this, "电话号码不能为空", Toast.LENGTH_LONG).show();
+					return;
+				}
+				//验证码判空处理       
+				if (TextUtils.isEmpty(etCode.getText().toString())) {
+					Toast.makeText(AtyLogin.this, "验证码不能为空", Toast.LENGTH_LONG).show();
+					return;
+				}
+				
+				new Login(MD5Tool.md5(etPhone.getText().toString()), etCode.getText().toString(), new Login.SuccessCallback() {
+					
+					@Override
+					public void onSuccess(String token) {
+						Config.cacheToken(AtyLogin.this, token);
+						Config.cachePhoneNum(AtyLogin.this, etPhone.getText().toString());
+						
+						Intent intent = new Intent(AtyLogin.this,AtyTimeline.class);
+						intent.putExtra(Config.KEY_TOKEN, token);
+						intent.putExtra(Config.KEY_PHONE_NUM, etPhone.getText().toString());
+						startActivity(intent);
+						
+						finish();
+					}
+				}, new Login.FailCallback() {
+					
+					@Override
+					public void onFail() {
+						Toast.makeText(AtyLogin.this, "登录失败，请重新登陆！", Toast.LENGTH_LONG).show();
+					}
+				});
+				
+			}
+		});
 	}
+	
+	
 }
